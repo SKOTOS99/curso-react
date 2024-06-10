519022b398353e09635ac31739b7be67b5482d31
SKOTOS99 <felipd95@hotmail.com>
2024-06-10 17:27:51 -0600

    implementacion de evento onClick

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -2,10 +2,19 @@ import PropTypes from "prop-types";
 
 
 export const CounterApp = ({numero}) => {
+
+    const count = ()=>{
+        let temo = 0;
+        temo = numero + 1;
+        console.log(temo)
+    }
     return(
         <>
             <h1>CounterApp</h1>
             <h2>{ numero }</h2>
+            <button onClick={ count }>
+                +1
+            </button>
         </>
     );
 }
