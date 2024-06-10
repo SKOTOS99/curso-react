5a2507689bdc60e37ef170607eb72eb628a3b34d
SKOTOS99 <felipd95@hotmail.com>
2024-06-10 12:20:26 -0600

    uso de funciones en jsx y renderizado de objetos en html

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,13 +1,25 @@
 
+const experiencias=()=>{
+    return 'mendoza';
+}
 
-
+/**
+ * NO se puede mandar el objeto completo a un elemento html, se debe desestructurar o mandar por atributo
+ * como objeto.mesagge
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export const FirstApp = ()=>{
+    const {message,tittle} = {
+        message:'jesus!',
+        tittle:'programador web'
+    };
     return (
         <>
-            <h1>Jesus!</h1>
-            <p>Soy un titulo</p>
+            <h1>{ message }!</h1>
+            <p>{tittle}</p>
+            <p>{experiencias()}</p>
         </>
-
     );
 }
 
