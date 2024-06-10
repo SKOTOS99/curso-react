80b82a813504481d3144c52af40c82b64a3d6852
SKOTOS99 <felipd95@hotmail.com>
2024-06-10 13:03:33 -0600

    se agrego archivo con estilos css

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {FirstApp} from "./FirstApp.jsx";
 import {HellowWord} from "./HellowWord.jsx";
+import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root'))
